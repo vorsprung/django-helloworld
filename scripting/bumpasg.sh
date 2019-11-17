cb6127274e5d54ba2c4f7906d7b29c4ccaf61a3f
jamie <jamie@linuxufo.com>
2019-11-17 22:11:16 +0000

    moved asg restart into seperate script file

---
+++ scripting/bumpasg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+asgname=$(terraform show| perl -ne 'print $1 if m#autoScalingGroupName/([^"]+)#'')
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asgname" --max-size=6 --min-size=6
+sleep 10
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name "$asgname" --max-size=3 --min-size=3
+
