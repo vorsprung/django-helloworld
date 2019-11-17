1e0200293409097b4a5266681168d0f6b2576a1d
jamie <jamie@linuxufo.com>
2019-11-17 22:04:57 +0000

    paths in scripting

--- scripting/stripper.sh
+++ scripting/stripper.sh
@@ -1,3 +1,3 @@
 mkdir minimal
-cp -r django-helloworld/scripting/launchersetup.sh django-helloworld/manage.py django-helloworld/helloworld django-helloworld/setup.py minimal
+cp -r scripting/launchersetup.sh manage.py helloworld setup.py minimal
 chmod 755 minimal/launchersetup.sh
